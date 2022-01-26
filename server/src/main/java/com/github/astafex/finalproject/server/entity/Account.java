e678bd339a61df876c90111bcddba0382a98a3be
Artem Astafev <astafev.ph@gmail.com>
2022-01-26 22:29:04 +0300

    Изменены методы возвращающие коллекции классов

--- server/src/main/java/com/github/astafex/finalproject/server/entity/Account.java
+++ server/src/main/java/com/github/astafex/finalproject/server/entity/Account.java
@@ -1,13 +1,21 @@
 package com.github.astafex.finalproject.server.entity;
 
 import com.github.astafex.finalproject.atm.entity.Card;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Value;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 @Value
 public class Account {
+    @Getter(AccessLevel.NONE)
     Set<Card> cards;
     String number;
     Balance balance;
+
+    public Stream<Card> getCards() {
+        return cards.stream();
+    }
 }
--- server/src/main/java/com/github/astafex/finalproject/server/entity/Client.java
+++ server/src/main/java/com/github/astafex/finalproject/server/entity/Client.java
@@ -1,12 +1,20 @@
 package com.github.astafex.finalproject.server.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Value;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 @Value
 public class Client {
-    Long clientId;
-    String fullName;
+    @Getter(AccessLevel.NONE)
     Set<Account> accounts;
+    long clientId;
+    String fullName;
+
+    public Stream<Account> getAccounts() {
+        return accounts.stream();
+    }
 }
